8b72a7f2143ab72182862cc142f2eb38cd6902ef
Qwerty <Qwerty@Qwerty-HP>
2015-02-05 17:46:48 -0300

    Vista Controlador + Login

--- src/ventanas/MenuIngresar.java
+++ src/ventanas/MenuIngresar.java
@@ -4,21 +4,22 @@
  * and open the template in the editor.
  */
 
-package web;
+package ventanas;
 
 import com.sun.imageio.plugins.jpeg.JPEG;
 import javax.swing.JOptionPane;
+import web.funciones;
 
 /**
  *
  * @author Qwerty
  */
-public class inicio extends javax.swing.JFrame {
+public class MenuIngresar extends javax.swing.JFrame {
 
     /**
      * Creates new form inicio
      */
-    public inicio() {
+    public MenuIngresar() {
         initComponents();
         setLocationRelativeTo(null);
     }
@@ -225,20 +226,20 @@ public class inicio extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuIngresar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuIngresar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuIngresar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuIngresar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                inicio ini = new inicio();
+                MenuIngresar ini = new MenuIngresar();
                 ini.setVisible(true);
             }
         });
